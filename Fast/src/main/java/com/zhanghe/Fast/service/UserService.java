64b7565a9f4d9dd9652697bb797e74eebbe72929
SmallBoys <554645932@qq.com>
2017-08-04 18:05:42 +0800

    开始添加后台菜单的代码

--- Fast/src/main/java/com/zhanghe/Fast/service/UserService.java
+++ Fast/src/main/java/com/zhanghe/Fast/service/UserService.java
@@ -2,6 +2,8 @@ package com.zhanghe.Fast.service;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.zhanghe.Fast.entity.Permission;
 import com.zhanghe.Fast.entity.Role;
 import com.zhanghe.Fast.entity.User;
@@ -14,4 +16,6 @@ public interface UserService {
     public List<Permission> getPermissionByUserId(Long id);
 
     public User getUserByUserName(String UserName);
+    
+    public List<Permission> getPermissionByUserName(String username);
 }
