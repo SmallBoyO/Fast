dcb3214fe1728d2eb1fc90dd408f861ecd3bac19
SmallBoys <554645932@qq.com>
2017-11-14 18:06:03 +0800

    添加角色列表分页，添加角色 功能

---
+++ Fast/src/main/java/com/zhanghe/Fast/mapper/RolePermissionMapper.java
@@ -0,0 +1,10 @@
+package com.zhanghe.Fast.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.zhanghe.Fast.entity.RolePermission;
+@Mapper
+public interface RolePermissionMapper extends BaseMapper<RolePermission> {
+
+}
